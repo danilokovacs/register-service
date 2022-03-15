2f991525f61c79b7b105923132e398497a5bc809
danilokovacs <danilo.kovacs@bandtec.com.br>
2022-03-15 11:04:05 -0300

    Searching route

--- src/main/kotlin/com/kovacs/register/service/controllers/RegisterController.kt
+++ src/main/kotlin/com/kovacs/register/service/controllers/RegisterController.kt
@@ -5,6 +5,7 @@ import com.kovacs.register.service.repositories.RegisterRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,5 +15,12 @@ class RegisterController (private val registerRepository: RegisterRepository) {
     @GetMapping("/products/frontend")
     fun frontend(): ResponseEntity<List<Product>>{
         return ResponseEntity.ok(this.registerRepository.findAll())
+    }
+
+    @GetMapping("/products/backend")
+    fun backend(
+        @RequestParam("s", defaultValue = "") s:String
+    ): ResponseEntity<List<Product>>{
+        return ResponseEntity.ok(this.registerRepository.search(s))
     }
 }
--- src/main/kotlin/com/kovacs/register/service/repositories/RegisterRepository.kt
+++ src/main/kotlin/com/kovacs/register/service/repositories/RegisterRepository.kt
@@ -2,6 +2,11 @@ package com.kovacs.register.service.repositories
 
 import com.kovacs.register.service.models.Product
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface RegisterRepository : JpaRepository<Product, Int> {
+
+    @Query("SELECT * FROM PRODUCT WHERE title like %?1%", nativeQuery = true)
+    fun search(s: String): List<Product>
+
 }
