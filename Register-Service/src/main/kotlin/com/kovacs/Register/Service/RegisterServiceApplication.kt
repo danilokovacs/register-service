a6997c04978928465b848168338dc0187addeb50
danilokovacs <danilo.kovacs@bandtec.com.br>
2022-03-15 09:53:16 -0300

    Spring Boot Template

---
+++ Register-Service/src/main/kotlin/com/kovacs/Register/Service/RegisterServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.kovacs.Register.Service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RegisterServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RegisterServiceApplication>(*args)
+}
---
+++ Register-Service/src/test/kotlin/com/kovacs/Register/Service/RegisterServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kovacs.Register.Service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
